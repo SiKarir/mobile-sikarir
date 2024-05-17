2122058721bc1ae3c87563f06e8c2f30899b3efe
Abdillah Sulthan Naufal Panggabean <abdillahsulthan12@gmail.com>
2024-05-17 13:01:23 +0700

    initial commit

---
+++ app/src/main/java/com/c241ps294/sikarir/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.c241ps294.sikarir
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
