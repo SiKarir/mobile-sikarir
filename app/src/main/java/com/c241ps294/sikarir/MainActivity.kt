41aef4a604a8708a708037b2a35876873ca491cd
Andre <112604705+andreshapradana@users.noreply.github.com>
2024-05-17 16:35:18 +0700

    setup view binding (#1)
    * test first commit
    
    * setup up viewbinding

--- app/src/main/java/com/c241ps294/sikarir/MainActivity.kt
+++ app/src/main/java/com/c241ps294/sikarir/MainActivity.kt
@@ -2,10 +2,13 @@ package com.c241ps294.sikarir
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.c241ps294.sikarir.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
