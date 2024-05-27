4fa603f3b2b9e1e04ed9dcaec50bbbb5828d79d1
Abdillah Sulthan Naufal Panggabean <abdillahsulthan12@gmail.com>
2024-05-27 23:35:25 +0700

    fix: main app compat activity

--- app/src/main/java/com/c241ps294/sikarir/ui/home/MainActivity.kt
+++ app/src/main/java/com/c241ps294/sikarir/ui/home/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.c241ps294.sikarir.ui.home
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.c241ps294.sikarir.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
