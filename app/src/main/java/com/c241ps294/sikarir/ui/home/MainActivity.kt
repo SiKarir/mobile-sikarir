41129fb7bd1e6ca355c8c3d544f1b7acfda6765a
Abdillah Sulthan Naufal Panggabean <abdillahsulthan12@gmail.com>
2024-05-28 01:03:34 +0700

    feat: xml login, register, and welcome

--- app/src/main/java/com/c241ps294/sikarir/ui/home/MainActivity.kt
+++ app/src/main/java/com/c241ps294/sikarir/ui/home/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.c241ps294.sikarir.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
