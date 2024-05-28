8519395345dca7f4af95bd306ba0391d6a08192b
Abdillah Sulthan Naufal Panggabean <abdillahsulthan12@gmail.com>
2024-05-29 01:47:15 +0700

    revise: main and catalog ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,17 @@ dependencies {
 
     //view-pager
     implementation("androidx.viewpager2:viewpager2:1.1.0")
-    //circural image view
+
+    //circural-image-view
     implementation ("de.hdodenhof:circleimageview:3.1.0")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.0")
+
+    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("androidx.fragment:fragment-ktx:1.7.1")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
