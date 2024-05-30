7a410f3b425b7c7618f7c2cda0f1a9a5338e962d
Andre <112604705+andreshapradana@users.noreply.github.com>
2024-05-30 17:02:48 +0700

    Feat: Dark Mode Settings (#5)
    * test first commit
    
    * setup up viewbinding
    
    * Home + Catalog Layouts
    
    * Activity setting finished
    
    * Feat: Dark Mode Finished
    
    * Feat: dark mode halfway completed

--- app/src/main/java/com/c241ps294/sikarir/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/c241ps294/sikarir/ui/settings/SettingsActivity.kt
@@ -4,6 +4,8 @@ import android.app.ActivityOptions
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
 import com.c241ps294.sikarir.R
 import com.c241ps294.sikarir.databinding.ActivitySettingsBinding
 import com.c241ps294.sikarir.ui.catalog.CatalogActivity
@@ -13,6 +15,7 @@ import com.c241ps294.sikarir.ui.settings.account.AccountActivity
 import com.c241ps294.sikarir.ui.settings.account.EditAccountActivity
 import com.c241ps294.sikarir.ui.welcome.WelcomeActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -22,6 +25,8 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
+
+
 
         bottomNavigationView = binding.bottomNavigation
         bottomNavigationView.selectedItemId = R.id.settings_page
@@ -63,5 +68,18 @@ class SettingsActivity : AppCompatActivity() {
         }
 
         setContentView(binding.root)
+
+
+        val switchTheme = findViewById<SwitchMaterial>(R.id.switch_theme)
+
+        switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switchTheme.isChecked = false
+            }
+        }
     }
 }
